8692c833a6c5bf81e230c8c10fd0c7425797ee08
alfuhigi <abajall@gmail.com>
2022-08-08 02:01:13 +0300

    support json api

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
+type Json struct {
+	Data string `json:"data"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there!")
+	data := Json{}
+	data.Data = "Hi there!"
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", handler)
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
