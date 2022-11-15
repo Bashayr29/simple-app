097f762e61d29d884a98ac1a5ce6b8e0849accfd
Bashayr Alabdullah <bshayr29@gmail.com>
2022-11-15 11:44:08 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there new pipeline build from github :)")
+    fmt.Fprintf(w, "Hi there new pipeline build from github add webhooks :)")
 }
 
 func main() {
